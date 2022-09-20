b7947a89808d7ba886a0d02b2a162f15cb995ad6
Kirill Allyanov <kir.allyanov@gmail.com>
2022-09-20 21:51:07 +0300

    first commit

---
+++ 41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Input Length");
+int m = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[m]; 
+Console.WriteLine(CountNum(array)); 
+
+int CountNum(int[] array)
+{
+    int count = 0; 
+    for(int i = 0; i < array.Length; i++)
+    {   
+        Console.WriteLine("Input Number");
+        array[i] = Convert.ToInt32(Console.ReadLine()); 
+        if(array[i] > 0)
+        {
+            count = count + 1; 
+        }
+    }
+    return count; 
+}
+
